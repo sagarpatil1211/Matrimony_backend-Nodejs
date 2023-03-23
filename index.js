fe88c9fc7ea18ab1823450a84d85e88760351025
Sagar <patilsagar1211@gmail.com>
2023-03-23 16:39:32 +0530

    changes

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ let express = require("express");
 let bodyparser = require("body-parser");
 let mongoose = require("mongoose");
 
-
 let app = express();
-
+app.use(express.static("assets"));
 app.use(bodyparser.json({limit : '50mb'})) ;
 app.use(bodyparser.urlencoded({limit:'50mb', extended: true}))
 app.use(express.json());
@@ -34,7 +33,7 @@ app.use("/admins", require("./routes/admins"));
 app.use("/states", require("./routes/states"));
 app.use("/districts", require("./routes/districts"));
 app.use("/login", require("./routes/login"));
-app.use("/busenesses", require("./routes/busenesses"));
+app.use("/busenesses", require("./routes/businesses"));
 app.use("/businesspay", require("./routes/businesspay"));
 
 
--- models/Business.js
+++ models/Business.js
@@ -6,14 +6,19 @@ let schema = new Schema(
         name : { type : String, required : true},
         ownername : { type : String, required : true},
         address : { type : String, required : true},
-        ownername : { type : String, required : true},
         townid : { type : String, required : true},
         tagline : { type : String, required : true},
         email : { type : String, required : true, unique : true},
         mobileno : { type : String, required : true, unique : true},
+        password : { type : String, required : true},
         website : { type : String, required : true},
-        logopath : { type : String, required : true},
+        logopath : { type : String },
         accountopeningdate : { type : Date, required : true},
-        ownername : { type : String, required : true},
+        accountexpdate : { type : Date},
+        status : { type : Boolean, required : true},
     }
 )
+
+let Business = mongoose.model("businesses", schema);
+
+module.exports = Business;
