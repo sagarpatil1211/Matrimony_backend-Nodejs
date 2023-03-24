77ba587ae8f0ec916b33598db3bb820e5806c65a
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-03-24 12:04:38 +0530

    change

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ app.use("/admins", require("./routes/admins"));
 app.use("/states", require("./routes/states"));
 app.use("/districts", require("./routes/districts"));
 app.use("/login", require("./routes/login"));
+app.use("/plans", require("./routes/plans"));
+
+
 
 
 app.listen(8081, ()=>{
