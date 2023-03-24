928424d468e85a3d8b24d256679486a20cb4cc25
Vivek Sutar <viveksutar26@gmail.com>
2023-03-24 13:44:56 +0530

    changes

--- models/Admin.js
+++ models/Admin.js
@@ -3,11 +3,6 @@ let Schema = mongoose.Schema;
 
 let schema = new Schema(
     {
-        username: { type: String, required: true, unique: true },
-        email: { type: String, required: true, unique: true },
-        mobileno: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        usertype: { type: String, required: true }
         name : { type : String , required : true },   
         username : { type : String , required : true },
         email : { type : String , required : true, unique : true},
